98c429bd05a45a05ca3f8bc90cb97d1d7e93880d
lxst <lxst@YEGOR>
2025-09-01 22:29:09 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Вас приветствует калькулятор!");
+
+                int x = 0;
+                int y = 0;
+
+                try
+                {
+                    Console.Write("Введите целое число. X = ");
+                    x = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Введите целое число. Y = ");
+                    y = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Ошибка! Входная строка имела неверный формат.");
+                    return;
+                }
+
+                catch (Exception ex)
+                {
+                    Console.Write("Неожиданная ошибка: " + ex.Message);
+                    return;
+                }
+
+                Console.WriteLine("Введите код операции: ");
+                Console.WriteLine($"\t1 - сложение;");
+                Console.WriteLine($"\t2 - вычитание;");
+                Console.WriteLine($"\t3 - произведение;");
+                Console.WriteLine($"\t4 - частное.");
+
+                Console.Write("Ваш выбор: ");
+                int userChoice = Convert.ToInt32(Console.ReadLine());
+                double result = 0;
+
+                try
+                {
+                    switch (userChoice)
+                    {
+                        case 1:
+                            result = x + y;
+                            break;
+
+                        case 2:
+                            result = x - y;
+                            break;
+
+                        case 3:
+                            result = x * y;
+                            break;
+
+                        case 4:
+                            result = (double)x / y;
+                            break;
+
+                        default:
+                            Console.WriteLine("Нет операции с указанным номером.");
+                            return;
+                    }
+                }
+
+                catch (OverflowException)
+                {
+                    Console.Write("Ошибка: число слишком большое или слишком маленькое");
+                    return;
+                }
+
+                catch (DivideByZeroException)
+                {
+                    Console.Write("Ошибка: деление на ноль.");
+                    return;
+                }
+
+                catch (Exception ex)
+                {
+                    Console.Write("Неожиданная ошибка: " + ex.Message);
+                    return;
+                }
+
+                Console.Write("Ваш результат: {0}.", result);
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Exception Handling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exception Handling")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3824c87d-0c41-4c5f-97e4-7fb232602d2e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
